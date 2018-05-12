0cb2aab69cddf1fb46beaf76401ef617f13e38aa
pascal <pascal@pascal-iMac>
2018-05-12 19:37:59 +0200

    Ajout du Readme.md et de commentaires dans bonjour.py

---
+++ Readme.md
@@ -0,0 +1,12 @@
+BONJOUR
+======
+
+# Description
+Programme en Python qui dit Bonjour à une liste de prénom contenue
+dans le fichier `prenoms.txt`.
+
+# Exécution
+Pour lancer le programme, taper :
+
+
+    python bonjour.py
--- bonjour.py
+++ bonjour.py
@@ -1,6 +1,13 @@
 #-*- coding: utf-8 -*-
+""" programme qui dit bonjour.
+    Il suffit d'écrire des prénoms dans le fichier prenoms.txt
+    et d'exécuter : 
+    python bonjour.py
+"""
 
+# fichier où sont contenus les prénoms
 fichier = open("prenoms.txt","r")
 
+# boucle de lecture des prénoms dans le fichier
 for prenom in fichier:
-    print ("Bonjour {}".format(prenom))
+    print ("Bonjour {}".format(prenom)) # écriture du bonjour
